de20ce4eb9ebfe511c2debe44be2474009178c0f
anilkumarnagaraj <anilkumar@in.ibm.com>
2024-05-21 18:38:13 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,5 +7,10 @@ terraform {
 }
 
 provider "ibm" {
-  # Configuration options
+  ibmcloud_api_key = var.ibmcloud_api_key
+  region           = var.region
+}
+
+resource "ibm_is_vpc" "vpc" {
+  name = var.vpc_name
 }

71090dde41eadb3726085550ca5b87a93b519910
anilkumarnagaraj <anilkumar@in.ibm.com>
2024-05-21 18:38:59 +0530

    Update provider.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
-resource "ibm_is_vpc" "vpc" {
-  name = var.vpc_name
+#resource "ibm_is_vpc" "vpc" {
+#  name = var.vpc_name
+#}
+
+
+data "ibm_code_engine_app" "code_engine_app" {
+    name = "application-01"
+    project_id = "42dd1d57-c71c-465c-bb5f-01815c0bc2d2"
 }
--- provider.tf
+++ provider.tf
@@ -7,10 +7,4 @@ terraform {
 }
 
 provider "ibm" {
-  ibmcloud_api_key = var.ibmcloud_api_key
-  region           = var.region
-}
-
-resource "ibm_is_vpc" "vpc" {
-  name = var.vpc_name
 }

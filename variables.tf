6ab4b1b2c4442cfbd46f7d1990d523e4f1c0c653
anilkumarnagaraj <anilkumar@in.ibm.com>
2025-06-27 07:29:16 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "ibmcloud_api_key" {
+  description = "IBM Cloud API Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "region" {
+  description = "Region where Code Engine project is deployed"
+  type        = string
+  default     = "us-east"
+}
+
+variable "project_id" {
+  description = "Code Engine project ID"
+  type        = string
+  default     = "e5511317-2c53-409a-b1aa-201e6eaaa2bd" 
+}
+
+variable "app_name" {
+  description = "Code Engine application name"
+  type        = string
+  default     = "application-e5" 
+}

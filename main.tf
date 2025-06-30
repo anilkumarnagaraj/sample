5b1bf2351290ac3de0601194e0ca02aa7bd68b89
anilkumarnagaraj <anilkumar@in.ibm.com>
2025-06-30 15:00:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,11 +3,20 @@
 #}
 
 
-data "ibm_code_engine_app" "code_engine_app" {
-  name       = var.app_name
-  project_id = var.project_id
+#data "ibm_code_engine_app" "code_engine_app" {
+#  name       = var.app_name
+#  project_id = var.project_id
+#}
+
+#output "app_details" {
+#  value = data.ibm_code_engine_app.code_engine_app
+#}
+
+data "ibm_resource_group" "resource_group" {
+   name = var.your_resource_group_name
 }
 
-output "app_details" {
-  value = data.ibm_code_engine_app.code_engine_app
+resource "ibm_code_engine_project" "code_engine_project_instance" {
+   name = var.project_name
+   resource_group_id = data.ibm_resource_group.resource_group.id
 }
